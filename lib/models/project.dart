deb6fc99d4effb5c3a38dc91219cd4fd257db9fd
Chandrashekhar <shekhar.kotekar@gmail.com>
2024-01-08 20:04:03 +0100

    models added

---
+++ lib/models/project.dart
@@ -0,0 +1,4 @@
+class Project {
+  final String title, description;
+  Project({required this.title, required this.description});
+}
---
+++ lib/models/recommendation.dart
@@ -0,0 +1,5 @@
+class Recommendation {
+  final String name, text;
+  final String? source;
+  Recommendation({required this.name, required this.text, this.source});
+}
