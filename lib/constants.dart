4ab9554c6f8b669a8f44ba229b0a01895724136b
Chandrashekhar <shekhar.kotekar@gmail.com>
2024-01-08 19:59:20 +0100

    constants added

---
+++ lib/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+Color primaryColor = Colors.blue.shade300;
+Color secondaryColor = Colors.green.shade400;
+
+const double defaultPadding = 20.0;
+const Duration defaultDuration = Duration(seconds: 1);
+const double maxWidth = 1440.0;
